35cbf2456b6d8f95482e6868440c04437c6a41be
SiuKam <ganzhaozhuo@gmail.com>
2019-05-27 15:44:05 +0800

    updated 20190527

--- merge_snippets.sh
+++ merge_snippets.sh
@@ -38,7 +38,7 @@ unset LC_ALL
 printf "\r\n127.0.0.1 localhost\r\n" >> $new_hosts_file
 printf "::1 localhost ip6-localhost ip6-loopback\r\n\r\n" >> $new_hosts_file
 
-cat snippets/??_*.txt >> $new_hosts_file
+cat hosts.out >> $new_hosts_file
 
 printf "\
 ## +-------------------------------------------------------------------+\r\n\
